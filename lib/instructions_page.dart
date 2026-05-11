32d1189fe0f388e0f72d92f09f2d1e784cdb2922
Kurt Lawrence Gil <gilkurtlawrence05@gmail.com>
2026-05-12 02:17:51 +0800

    Update instructions page and main app

--- lib/instructions_page.dart
+++ lib/instructions_page.dart
@@ -8,6 +8,8 @@ import 'package:shimmer/shimmer.dart';
 import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
 
 import 'look_engine.dart';
+import 'config/makeup_look_config.dart' as guide_config;
+import 'config/makeup_look_configs.dart';
 import 'openai_service.dart';
 import 'painters/base_prep_guide_painter.dart';
 import 'painters/eyebrow_guide_painter.dart';
@@ -47,6 +49,8 @@ class InstructionsPage extends StatefulWidget {
 }
 
 class _InstructionsPageState extends State<InstructionsPage> {
+  late final guide_config.MakeupLookConfig _config;
+
   // AI-related state
   bool _loadingAI = false;
   String? _aiError;
@@ -70,6 +74,8 @@ class _InstructionsPageState extends State<InstructionsPage> {
   @override
   void initState() {
     super.initState();
+
+    _config = MakeupLookConfigs.get(widget.selectedPreset);
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
       _generateAIInstructions();
@@ -264,7 +270,7 @@ class _InstructionsPageState extends State<InstructionsPage> {
         prefix: 'eyeshadow_guide_',
         painter: EyeshadowGuidePainter(
           face: widget.detectedFace!,
-          preset: widget.selectedPreset,
+          config: _config,
           palette: EyeshadowGuidePalette(
             lidColor: widget.look.eyeshadowColor.withOpacity(0.95),
             creaseColor: widget.look.eyeshadowColor.withOpacity(0.75),
@@ -295,7 +301,7 @@ class _InstructionsPageState extends State<InstructionsPage> {
         prefix: 'eyeliner_guide_',
         painter: EyelinerGuidePainter(
           face: widget.detectedFace!,
-          preset: widget.selectedPreset,
+          config: _config,
           guideColor: const Color(0xFFFF4D97),
         ),
       );
